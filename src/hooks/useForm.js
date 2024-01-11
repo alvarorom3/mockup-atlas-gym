d29980873fb8f4141bb118eacf03846599edfca1
Álvaro Romero <alvarorom333@gmail.com>
2024-01-11 03:24:22 -0300

    Change: .github/workflows/viteDeploy.yml - adding github secrets

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,67 +1,64 @@
-import { useState } from "react"
-import emailjs from "@emailjs/browser"
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 export function useForm(initialForm, validateForm) {
-    const [form, setForm] = useState(initialForm)
-    const [errors, setErrors] = useState({})
-    const [loading, setLoading] = useState(false)
-    const [response, setResponse] = useState(null)
+	const [form, setForm] = useState(initialForm);
+	const [errors, setErrors] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [response, setResponse] = useState(null);
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setForm({
-            ...form,
-            [name]: value,
-        })
-    }
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setForm({
+			...form,
+			[name]: value,
+		});
+	}
 
-    function handleBlur(e) {
-        handleChange(e)
-        setErrors(validateForm(form))
-    }
+	function handleBlur(e) {
+		handleChange(e);
+		setErrors(validateForm(form));
+	}
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setErrors(validateForm(form))
+	function handleSubmit(e) {
+		e.preventDefault();
+		setErrors(validateForm(form));
 
-        if (Object.keys(errors).length === 0) {
-            setLoading(true)
+		if (Object.keys(errors).length === 0) {
+			setLoading(true);
 
-            emailjs
-                .send(
-                    "service_i0ktc2g",
-                    "template_gz2pa6m",
-                    form,
-                    "CMurzMy_6kLiOPNiq"
-                )
-                .then(
-                    result => {
-                        setLoading(false)
-                        setResponse(true)
-                        console.log(result.text)
-                        console.log("Enviado ✔")
-                        setForm(initialForm)
-                        setTimeout(() => {
-                            setResponse(false)
-                        }, 3000)
-                    },
-                    error => {
-                        console.log(error.text)
-                    }
-                )
-        } else {
-            alert("Su mensaje no puede enviarse si aún contiene errores.")
-            return
-        }
-    }
+			emailjs.send(serviceId, templateId, form, publicKey).then(
+				result => {
+					setLoading(false);
+					setResponse(true);
+					console.log(result.text);
+					console.log('Enviado ✔');
+					setForm(initialForm);
+					setTimeout(() => {
+						setResponse(false);
+					}, 3000);
+				},
+				error => {
+					console.log(error.text);
+				}
+			);
+		} else {
+			alert('Su mensaje no puede enviarse si aún contiene errores.');
+			return;
+		}
+	}
 
-    return {
-        form,
-        errors,
-        loading,
-        response,
-        handleBlur,
-        handleChange,
-        handleSubmit,
-    }
+	return {
+		form,
+		errors,
+		loading,
+		response,
+		handleBlur,
+		handleChange,
+		handleSubmit,
+	};
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/mockup-atlas-gym/',
+	base: '/mockup-atlas-gym',
 });
